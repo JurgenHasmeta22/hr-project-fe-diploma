e6bd0f263f1c92d50a501688630702b0d87c45f1
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2023-08-10 19:42:41 +0200

    bugfix

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -18,20 +18,6 @@ const Dashboard = () => {
 		<Box m="20px">
 			<Box display="flex" justifyContent="space-between" alignItems="center">
 				<Header title="Dashboard" subtitle="Welcome to your dashboard" />
-				<Box>
-					<Button
-						sx={{
-							backgroundColor: colors.blueAccent[700],
-							color: colors.grey[100],
-							fontSize: '14px',
-							fontWeight: 'bold',
-							padding: '10px 20px'
-						}}
-					>
-						<DownloadOutlinedIcon sx={{ mr: '10px' }} />
-						Download Reports
-					</Button>
-				</Box>
 			</Box>
 			<Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gridAutoRows="140px" gap="20px">
 				<Box
@@ -132,14 +118,6 @@ const Dashboard = () => {
 							$48,352 revenue generated
 						</Typography>
 						<Typography>Includes extra misc expenditures and costs</Typography>
-					</Box>
-				</Box>
-				<Box gridColumn="span 4" gridRow="span 2" sx={{ backgroundColor: colors.primary[400] }}>
-					<Typography variant="h5" fontWeight="600" sx={{ padding: '30px 30px 0 30px' }}>
-						Sales Quantity
-					</Typography>
-					<Box height="250px" mt="-20px">
-						<BarChart isDashboard={true} />
 					</Box>
 				</Box>
 			</Box>
