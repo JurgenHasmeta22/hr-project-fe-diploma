37b4882a4719323463e539267b063456eb2c0f2c
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-03-03 17:09:38 +0100

    fix projects

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -8,17 +8,17 @@ const projectsController = {
     },
     getProject: async (projectId: string) => {
         return await axios
-            .get(`https://localhost:7006/Projekt/${projectId}`)
+            .get(`https://localhost:7006/Projekt/GetById/${projectId}`)
             .then((response) => response.data);
     },
     deleteProject: async (projectId: any) => {
         return await axios
-            .delete(`https://localhost:7006/Projekt/${projectId}`)
+            .delete(`https://localhost:7006/Projekt/Delete/${projectId}`)
             .then((response) => response.data);
     },
     updateProject: async (projectId: any, model: any) => {
         return await axios
-            .put(`https://localhost:7006/Projekt/${projectId}`, model)
+            .put(`https://localhost:7006/Projekt/Put/${projectId}`, model)
             .then((response) => response.data);
     },
     addProject: async (model: any) => {
@@ -26,18 +26,18 @@ const projectsController = {
             .post('https://localhost:7006/Projekt', model)
             .then((response) => response.data);
     },
-    assignProjectToUser: async (userId: any, projectId: any, model: any) => {
+    assignProjectToUser: async (userId: any, projektId: any, model: any) => {
         return await axios
             .post(
-                `https://localhost:7006/User/${userId}/Projekt/${projectId}`,
+                `https://localhost:7006/User/AssignProjectToUser/${userId},${projektId}`,
                 model,
             )
             .then((response) => response.data);
     },
-    deleteProjectToUser: async (userId: any, projectId: any) => {
+    deleteProjectToUser: async (userId: any, projektId: any) => {
         return await axios
             .delete(
-                `https://localhost:7006/User/${userId}/Projekt/${projectId}`,
+                `https://localhost:7006/User/DeleteMappedProjectToUser/${userId},${projektId}`,
             )
             .then((response) => response.data);
     },
