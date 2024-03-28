87beaffc3488636831189218ee3038993086e00e
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-03-28 10:54:14 +0100

    fixed localstorage custom hook bugs

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,14 +1,14 @@
 import { Outlet, Navigate } from "react-router-dom";
-import useLocalStorage from "~/hooks/useLocalStorage";
+import { useLocalStorage } from "~/hooks/useLocalStorage";
+import { ReactNode } from "react";
 
 interface UserData {
     token: string;
 }
 
-const PrivateRoutes = () => {
-    const [userData] = useLocalStorage<UserData>("user");
-
-    return userData && userData.token ? <Outlet /> : <Navigate to="/login" />;
+const PrivateRoutes: React.FC = () => {
+    const { value } = useLocalStorage("user");
+    return value && value.token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoutes;
